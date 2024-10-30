e3ab549417a60397d1a29f0544d6aaf410630eb8
Graeme Turney <graemeturney@gmail.com>
2024-10-30 14:12:48 -0700

    Initialize initial game pieces
    - board with tiles, that can hold cards
    - hand that can also hold cards
    - proper state to come

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+import { css } from '@pigment-css/react';
 import { Board } from '../components/Board';
+import { Card } from '../components/Card';
+import { Hand } from '../components/Hand';
 
 export default function Page() {
     return (
         <div>
             <main>
-                <div>Hello :)</div>
+                <div className={css({ fontSize: '30px' })}>Triple Tri</div>
+                <Card owner='red' />
+                <Hand owner='red' />
                 <Board />
             </main>
             <footer></footer>
