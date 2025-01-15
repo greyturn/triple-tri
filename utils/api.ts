bf827df6e712c91064970bdf8f556d75add8e327
Graeme Turney <graemeturney@gmail.com>
2025-01-15 14:09:29 -0800

    Fix api on prod env
    - needed to differentiate local and prod

---
+++ utils/api.ts
@@ -0,0 +1,7 @@
+export const IS_DEV = process.env.NEXT_PUBLIC_VERCEL_ENV === 'development'
+export const DOMAIN = IS_DEV ? 'localhost:3000' : process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL;
+export const PROTOCOL = IS_DEV ? 'http://' : 'https://';
+
+export function apiFetch(url: string) {
+    return fetch(`${PROTOCOL}${DOMAIN}${url}`)
+}
