6179476cbc305aebe327a3069bb0374c24e20725
Graeme Turney <graemeturney@gmail.com>
2025-01-15 14:13:15 -0800

    use next_public_vercel_url instead

--- utils/api.ts
+++ utils/api.ts
@@ -1,5 +1,5 @@
 export const IS_DEV = process.env.NEXT_PUBLIC_VERCEL_ENV === 'development'
-export const DOMAIN = IS_DEV ? 'localhost:3000' : process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL;
+export const DOMAIN = IS_DEV ? 'localhost:3000' : process.env.NEXT_PUBLIC_VERCEL_URL;
 export const PROTOCOL = IS_DEV ? 'http://' : 'https://';
 
 export function apiFetch(url: string) {
